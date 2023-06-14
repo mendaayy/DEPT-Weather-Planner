aa7674889c3c7fc4f97ec0677e45b2cba4443193
mendy liang <mendyliang@mendys-MacBook-Pro.local>
2023-06-14 04:07:12 +0200

    final touches

--- weather-planner/next.config.js
+++ weather-planner/next.config.js
@@ -1,6 +1,13 @@
 const nextConfig = {
   images: {
-    domains: ['images.unsplash.com']
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/photo-*',
+      },
+    ],
   },
 };
 

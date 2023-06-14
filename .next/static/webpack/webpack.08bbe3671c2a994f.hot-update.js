404979d66b05cad2157db93e1b7ccf934957a3ec
mendy liang <mendyliang@mendys-MacBook-Pro.local>
2023-06-14 06:58:00 +0200

    package file changed

--- .next/static/webpack/webpack.08bbe3671c2a994f.hot-update.js
+++ .next/static/webpack/webpack.08bbe3671c2a994f.hot-update.js
@@ -35,7 +35,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ 
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "cd7a074b2903260c"; }
+/******/ 	__webpack_require__.h = function() { return "2c0596f75484025e"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/global */
